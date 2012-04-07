e67e42df347d830341e3a850dd81f54cf2103a4d
John P. Bloch <johnpbloch@gmail.com>
2012-04-07 19:54:36 -0400

    Add a monthly cron job to flush the keys.

--- lib/WP_MultiServer_Migration.php
+++ lib/WP_MultiServer_Migration.php
@@ -1,28 +1,51 @@
 <?php
 
-class WP_MultiServer_Migration {
+class WP_MultiServer_Migration
+{
 
-	public static function bootstrap() {
+	public static function bootstrap()
+	{
 		add_action( 'init', array( __CLASS__, 'init' ), 11 );
 		add_action( 'parse_request', array( __CLASS__, 'maybeDoManifest' ), 1 );
+		add_action( 'wpmsm_create_new_keys', array( 'WP_MSM_OpenSSL', 'generate_keys' ) );
+		add_action( 'cron_schedules', array( __CLASS__, 'create_monthly_schedule' ) );
 	}
 
 	/**
 	 * @global WP $wp 
 	 */
-	public static function init() {
+	public static function init()
+	{
 		global $wp;
 		add_rewrite_rule( 'wpmsm-manifest\.json$', 'index.php?wpmsmm=1', 'top' );
-		$wp->add_query_var('wpmsmm');
+		$wp->add_query_var( 'wpmsmm' );
+		if( !wp_next_scheduled( 'wpmsm_create_new_keys' ) )
+		{
+			wp_schedule_event( time(), 'monthly', 'wpmsm_create_new_keys' );
+		}
 	}
 
-	public static function maybeDoManifest( WP &$wp ) {
-		if( empty( $wp->query_vars['wpmsmm'] ) ) {
+	public static function maybeDoManifest( WP &$wp )
+	{
+		if( empty( $wp->query_vars['wpmsmm'] ) )
+		{
 			return;
 		}
 		header( 'Content-type: application/json' );
 		require( WP_MULTISERVER_VAR . '/manifest.php' );
 		exit;
+	}
+
+	public static function create_monthly_schedule( array $schedules )
+	{
+		if( empty( $schedules['monthly'] ) )
+		{
+			$schedules['monthly'] = array(
+				'interval' => (60 * 60 * 24 * 30),
+				'display' => __( 'Monthly', 'WordPress-MultiServer-Migration' ),
+			);
+		}
+		return $schedules;
 	}
 
 }
