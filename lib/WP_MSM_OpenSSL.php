e1a61760bfb7b1735d4be9bdcebfb96fd6351379
John P. Bloch <johnpbloch@gmail.com>
2012-04-08 09:24:05 -0400

    Add documentation to the generate keys method.
    Always return a value too.

--- lib/WP_MSM_OpenSSL.php
+++ lib/WP_MSM_OpenSSL.php
@@ -3,6 +3,18 @@
 class WP_MSM_OpenSSL
 {
 
+	/**
+	 * Generates a pair of public and private keys for the plugin to use.
+	 * 
+	 * Keys can either be returned without saving them to the plugin's options
+	 * object or can be saved directly to the plugin's options object. Used
+	 * internally. This is probably never going to need to be used directly,
+	 * since it's set up at installation and then fresh keys are re-generated
+	 * every 30 days.
+	 * 
+	 * @param bool $saveDirectly Save the keys directly to options. False to return keys.
+	 * @return array|bool An array containing keys if $saveDirectly is true, boolean indicating save success otherwise. 
+	 */
 	public static function generate_keys( $saveDirectly = true )
 	{
 		$pkey_resource = openssl_pkey_new( array( 'private_key_bits' => 2048 ) );
@@ -16,7 +28,7 @@ class WP_MSM_OpenSSL
 		$options = WP_MSM_Options::instance();
 		$options->publicSignature = $public_key;
 		$options->privateSignature = $private_key;
-		$options->update();
+		return $options->update();
 	}
 
 }
