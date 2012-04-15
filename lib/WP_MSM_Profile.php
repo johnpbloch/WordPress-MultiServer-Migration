ee4e5dbe32240a23388478eee77cc1d62b7ece01
John P. Bloch <johnpbloch@gmail.com>
2012-04-15 09:44:51 -0400

    Add a `_toArray' method to WP_MSM_Profile so we can export the values as an array.
    This is mostly so PHP doesn't try to instantiate our objects (when they're deserialized after WP's autoload of options) before our autoloader is registered.

--- lib/WP_MSM_Profile.php
+++ lib/WP_MSM_Profile.php
@@ -8,15 +8,31 @@ class WP_MSM_Profile
 	public $dbTablesToExclude = array( );
 	public $canExport = true;
 	public $description = '';
+	protected $defaults = array( );
 
 	function __construct( array $settings )
 	{
 		$defaults = get_object_vars( $this );
+		$this->defaults = array_keys( $defaults );
 		$settings = array_merge( $defaults, array_intersect_key( $settings, $defaults ) );
 		foreach( $settings as $key => $setting )
 		{
 			$this->$key = $setting;
 		}
+	}
+
+	/**
+	 * Returns the profile settings as an array.
+	 * @return array The profile settings
+	 */
+	public function _toArray()
+	{
+		$toReturn = array( );
+		foreach( $this->defaults as $key )
+		{
+			$toReturn[$key] = $this->$key;
+		}
+		return $toReturn;
 	}
 
 }
